6efe4fc55047436c2f36a9324a66b500610aadee
TsT <tst2005@gmail.com>
2017-07-07 14:45:26 +0200

    expand groups to users

--- test.lua
+++ test.lua
@@ -1,5 +1,9 @@
 local tprint = require "tprint"
 
+local cfg;cfg = {inline=false, seen=setmetatable({}, {__newindex=function() end, __index=function() return nil end})}
+-- recursivefound = function(t, lvl, cfg) cfg.seen[t]=nil return tprint(t,cfg) end}
+
+
 if false then
 	do
 	local lpeg = require "lulpeg"
@@ -18,13 +22,11 @@ local ast = gitolite2ast( data )
 
 local groups = {}
 local function indexgroups(ast)
-	for _i, line in pairs(ast) do
+	for i, line in pairs(ast) do
 		if atype(line)=="GroupDefLine" then
-			--print(tprint(line, {inline=false}))
 			assert(atype(line[1])=="Group")
 			assert(atype(line[2])=="Members")
-			--print(line[1][1], tprint(line[2], {inline=true}))
-			print("index group:", line[1][1])
+			--print("index group:", line[1][1])
 			groups[line[1][1]] = line[2]
 		end
 	end
@@ -33,25 +35,64 @@ local function expandgroup2users(src)
 	local r = {}
 	for groupname, members in pairs(src) do
 		assert(atype(members)=="Members")
-		for _i, u_or_g in ipairs(members) do
+		for i, u_or_g in ipairs(members) do
 			assert(atype(u_or_g)=="User" or atype(u_or_g)=="Group")
 			if atype(u_or_g)=="Group" then
-				print("something to do")
-				--table.remove()
+				--print(groupname, "something to do")
+				--print(groupname, "replace i="..i, "groupname=", u_or_g[1])
+				members[i]=src[u_or_g[1]]
 			end
 		end
 	end
+	for groupname, members in pairs(src) do
+		assert(atype(members)=="Members")
+		--for i, u_or_g in ipairs(members) do
+		local i=0
+		while true do
+			i=i+1
+			local u_or_g = members[i]
+			if u_or_g ==nil then break end
+			assert(atype(u_or_g)=="User" or atype(u_or_g)=="Members")
+			if atype(u_or_g)=="Members" then
+				table.remove(members, i)
+				for _i, ug in ipairs(u_or_g) do
+					table.insert(members, i, ug)
+				end
+			end
+		end
+		r[groupname] = members
+	end
 	return r
+end
+local function applygroups(t)
+	if type(t)=="table" then
+		if atype(t)=="GroupDefLine" then return t end
+		for i, v in ipairs(t) do
+			--print(i,v)
+			if atype(v) == "Group" then
+				local groupname = v[1]
+				assert(groups[groupname])
+--				print("replace", groupname)
+				t[i]=groups[groupname]
+			end
+		end
+		for k, v in pairs(t) do
+			--if type(k)~="number" then
+				applygroups(v)
+			--end
+		end
+	end
+	return t
 end
 
 indexgroups(ast)
-expandgroup2users(groups)
+groups = expandgroup2users(groups)
+applygroups(ast)
 
---groups = expandgroup2users(groups)
+--print(tprint(ast, cfg))
 
---[[
+
 local gitolite = ast2gitolite(ast)
 io.stdout:write( gitolite )
-]]--
 
 --print(require"mini.tprint.better"(x, {inline=false}))
